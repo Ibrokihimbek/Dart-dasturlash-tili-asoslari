de6cd114bacb78d670a5c7ee0332db64c3ef7f44
Ibrokhimbek <ibrohim9902@gmail.com>
2022-09-01 21:51:30 +0500

    Direktor tashrifi Robocontest-0004

---
+++ Robocontest_0004.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(List<String> args) async {
+  Iterable<int> nums =
+      stdin.readLineSync()!.split(" ").map((e) => int.parse(e));
+
+  var writer = stdout.nonBlocking;
+  writer.write(nums.elementAt(0) * nums.elementAt(1));
+
+  await writer.close();
+  exit(0);
+}
